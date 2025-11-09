7914696eb792a59a513dd138b8d98b64613eaf1a
Mari Kathleen Abdon <kathabdon06@gmail.com>
2025-11-09 18:06:28 -0500

    fully working app, styling time recoring mui is done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,55 @@
-import { Button } from "@mui/material";
-import React from "react";
-
+import { Button, Stack, Typography, Box } from "@mui/material";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import { Link } from "react-router-dom";
+import bgImage from "../assets/background.jpg";
 function HomePage() {
    return (
-      <div>
-         <Button variant="contained"> Click me</Button>
-      </div>
+      <Stack
+         direction="row"
+         justifyContent="center"
+         minHeight="80vh"
+         marginY="auto"
+      >
+         <Box padding={16}>
+            <div>
+               <Stack direction="row" alignItems="center" spacing={1}>
+                  <AssignmentIcon />
+                  <Typography variant="h4" fontWeight="medium">
+                     Task Manager
+                  </Typography>
+               </Stack>
+
+               <Stack marginY={10} spacing={2}>
+                  <Typography variant="h3">
+                     Welcome to my <br />
+                     Task Manager Project!
+                  </Typography>
+                  <Typography variant="subtitle1" color="GrayText">
+                     Built by Spiderman
+                  </Typography>
+               </Stack>
+
+               <Button
+                  size="large"
+                  variant="contained"
+                  component={Link}
+                  to="/tasks"
+               >
+                  See my tasks
+               </Button>
+            </div>
+         </Box>
+         <Box
+            sx={{
+               backgroundImage: `url(${bgImage})`,
+               backgroundSize: "cover",
+               backgroundPosition: "center",
+               width: "50%",
+               margin: 8,
+               borderRadius: 8,
+            }}
+         ></Box>
+      </Stack>
    );
 }
 
